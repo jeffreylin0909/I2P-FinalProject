d283b55fb234766d739dd6f00d620b6dc18c4763
林哲宇 <56347663+jeffreylin0909@users.noreply.github.com>
2023-06-17 18:18:33 +0800

    test

--- src/player/minimax.cpp
+++ src/player/minimax.cpp
@@ -41,7 +41,7 @@ void read_board(std::ifstream& fin) {
 void write_valid_spot(std::ofstream& fout) {
   // Keep updating the output until getting killed.
   while(true) {
-    auto move = Minimax::get_move(root, 10);
+    auto move = Minimax::get_move(root, 12);
     fout << move.first.first << " " << move.first.second << " "\
          << move.second.first << " " << move.second.second << std::endl;
     
