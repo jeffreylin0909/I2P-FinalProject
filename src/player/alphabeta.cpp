e018db8239c35c4203a3bea9663528107ae51268
林哲宇 <56347663+jeffreylin0909@users.noreply.github.com>
2023-06-17 21:25:38 +0800

    update

--- src/player/alphabeta.cpp
+++ src/player/alphabeta.cpp
@@ -3,7 +3,7 @@
 
 #include "../config.hpp"
 #include "../state/state.hpp"
-#include "../policy/minimax.hpp"
+#include "../policy/alphabeta.hpp"
 
 
 State* root;
@@ -41,7 +41,7 @@ void read_board(std::ifstream& fin) {
 void write_valid_spot(std::ofstream& fout) {
   // Keep updating the output until getting killed.
   while(true) {
-    auto move = Minimax::get_move(root, 4);
+    auto move = Alphabeta::get_move(root, 8);
     fout << move.first.first << " " << move.first.second << " "\
          << move.second.first << " " << move.second.second << std::endl;
     
---
+++ src/policy/alphabeta.cpp
@@ -0,0 +1,52 @@
+#include <cstdlib>
+#include <climits>
+#include <algorithm>
+#include <iostream>
+
+#include "../state/state.hpp"
+#include "./alphabeta.hpp"
+
+
+/**
+ * @brief Randomly get a legal action
+ * 
+ * @param state Now state
+ * @param depth You may need this for other policy
+ * @return Move 
+ */
+int minimaxval(State *state, int depth,int me,int alpha,int beta){
+	if (depth==0){
+		return state->evaluate();
+	}
+	int ret;
+	if (state->player==me){
+		ret=INT_MIN;
+		for (Move M:state->legal_actions){
+			alpha=ret=std::max(ret,minimaxval(state->next_state(M),depth-1,me,ret,INT_MAX));
+			if (alpha>=beta){
+				break;
+			}
+		}
+	}else {
+		ret=INT_MAX;
+		for (Move M:state->legal_actions){
+			beta=ret=std::min(ret,minimaxval(state->next_state(M),depth-1,me,INT_MIN,ret));
+			if (alpha>=beta){
+				break;
+			}
+		}
+	}
+	return ret;
+}
+Move Alphabeta::get_move(State *state, int depth){
+	Move ret;
+	int Max=INT_MIN,tmp;
+	for (Move M:state->legal_actions){
+		tmp=minimaxval(state->next_state(M),depth-1,state->player,Max,INT_MAX);
+		if (tmp>Max){
+			ret=M;
+			Max=tmp;
+		}
+	}
+	return ret;
+}
